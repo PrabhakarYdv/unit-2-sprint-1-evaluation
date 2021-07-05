062d477b4cfc771b4b1bedaed595c21169ff4037
Prabhakar <prabhakarkr802@gmail.com>
2021-07-05 15:58:38 +0530

    Unit-2 Evaluation(MASAI School)

--- app/src/main/java/com/prabhakar/ft_and_03u_2c_1/HomeActivity.java
+++ app/src/main/java/com/prabhakar/ft_and_03u_2c_1/HomeActivity.java
@@ -1,14 +1,21 @@
 package com.prabhakar.ft_and_03u_2c_1;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class HomeActivity extends AppCompatActivity {
+    TextView greet_name;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        greet_name = findViewById(R.id.greet_name);
+        Intent intent = getIntent();
+        String name = intent.getStringExtra("name");
+        greet_name.setText(name);
     }
 }
--- app/src/main/java/com/prabhakar/ft_and_03u_2c_1/SignUpActivity.java
+++ app/src/main/java/com/prabhakar/ft_and_03u_2c_1/SignUpActivity.java
@@ -1,14 +1,31 @@
 package com.prabhakar.ft_and_03u_2c_1;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SignUpActivity extends AppCompatActivity {
+    TextView sign_up;
+    EditText editText_name;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+        sign_up = findViewById(R.id.sign_up_btn);
+        editText_name = findViewById(R.id.name);
+        sign_up.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SignUpActivity.this, HomeActivity.class);
+                String name = editText_name.getText().toString();
+                intent.putExtra("name", name);
+                startActivity(intent);
+            }
+        });
     }
 }
